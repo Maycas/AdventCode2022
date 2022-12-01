30ecb46ec88771472e2004c6904836e32bb92db5
Marc Maycas <marc.maycas@gmail.com>
2022-12-01 19:21:12 +0100

    Day 1 documented

--- Day 1 - Calorie Counting/calories.py
+++ Day 1 - Calorie Counting/calories.py
@@ -1,15 +1,49 @@
 FILE_DIR = './calories.txt'
 TOP = 3
 
-def read_list(file_directory):
+def read_list(file_directory: str) -> list:
     '''
-    Reads a file inside a directory
+    Reads a file inside a directory and returns a list 
+    containing an item per line that has been read.
+
+    Parameters
+    ----------
+    file_directory: str, mandatory
+        The directory where the file to read the lines is 
+        stored
+
+    Returns
+    ----------
+    lines: list
+        List containing a line per index
+
+    Raises
+    ----------
+    FileNotFoundError
+        If the file doesn't exist in the given directory
     '''
     with open(file_directory, 'r') as file:
         lines = [line.strip('\n') for line in file.readlines()]
     return lines
 
-def format_calories_list(file_directory):
+def format_calories_list(file_directory: str) -> list:
+    '''
+    Reads a file inside a directory and returns a 
+    formatted list where each position contains a list
+    of the item calories items carried by each elf.
+
+    Parameters
+    ----------
+    file_directory: str, mandatory
+        The directory where the file to read the lines is 
+        stored.
+
+    Returns
+    ----------
+    result: list
+        Item calories items carried by each elf.
+            E.g. [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+    '''
     lines = read_list(file_directory)
     
     result = []
@@ -25,7 +59,24 @@ def format_calories_list(file_directory):
 
     return result    
 
-def maximum_calories_carrying_elf(elf_calories_list):
+def maximum_calories_carrying_elf(elf_calories_list: list) -> tuple:
+    '''
+    Given a list of item calories per each elf, returns a
+    tuple with the elf carrying the maximum number of calories
+    and the sum of calories for all the items
+
+    Parameters
+    ----------
+    elf_calories_list: list, mandatory
+        A nested list where in each position there's a list
+        containing the calories for each item in the list
+
+    Returns
+    ----------
+    tuple
+        Contains the elf carrying the highest amount of item
+        calories and the calories sum
+    '''
     elf_with_max_calories = 0
     max_calories = 0
     for elf, calories in enumerate(elf_calories_list):
@@ -37,7 +88,33 @@ def maximum_calories_carrying_elf(elf_calories_list):
     
     return elf_with_max_calories, max_calories
 
-def maximum_calories_carrying_top_X_elves(elf_calories_list, X):
+def maximum_calories_carrying_top_X_elves(elf_calories_list: list, X: int = 1) -> tuple[list]:
+    '''
+    Returns a tuple containing 2 lists:
+        1 - The list of the top X elves carrying the items with the highest
+        amount of calories
+        
+        2 - The sum of calories per each elf
+
+    Each index in the lists corresponds to the same elf.
+
+    Parameters
+    ----------
+    elf_calories_list: list, mandatory
+        A nested list where in each position there's a list
+        containing the calories for each item in the list
+
+    X: int, optional
+        Number of top elves to return as result. When not 
+        informed is equal to 1
+
+     Returns
+    ----------
+    tuple[list]
+        List of tuples with the top elves carrying the highest 
+        amount of items and another one with the calories sum
+        for each elf.
+    '''
     elves = []
     total_calories_carried_by_top_elves = []
     for _ in range(X):
@@ -51,7 +128,30 @@ def maximum_calories_carrying_top_X_elves(elf_calories_list, X):
     return elves, total_calories_carried_by_top_elves
 
 
-def show_info(file_directory, top = 1):
+def show_info(file_directory: str, top: int = 1) -> dict:
+    '''
+    Reads a file inside a directory and returns a dictionary
+    with a summary on the top elves carrying the biggest sum of 
+    item calories.
+
+    Parameters
+    ----------
+    file_directory: str, mandatory
+        The directory where the file to read the lines is 
+        stored
+
+    top: int, optional
+        Number of top elves to return as result. When not 
+        informed is equal to 1
+
+    Returns
+    ----------
+    dict: With the following keys
+        total_elves: Number of elves going into expedition
+        top_elves_with_max_calories: List of top calory carrying elves
+        total_calories_carried_by_top_elves: List of the sum of all calories carried by each elf
+        sum_total_calories: Sum of all the calories carried by the top elves
+    '''
     elf_calories_list = format_calories_list(file_directory)
     total_elves = len(elf_calories_list)
 
@@ -64,4 +164,4 @@ def show_info(file_directory, top = 1):
         'sum_total_calories': sum(total_calories_carried_by_top_elves)
     }
 
-print(show_info(FILE_DIR))
+print(show_info(FILE_DIR, TOP))
